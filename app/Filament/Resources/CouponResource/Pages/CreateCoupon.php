5cc1e94c05b2ffecd64b3351f7114c7e0cb43dea
Csikós Zsolt <csikoszs21@gmail.com>
2024-09-05 09:29:05 +0200

    fix: hiba kezelés (nincs kapcsolat a 3 féllel)

--- app/Filament/Resources/CouponResource/Pages/CreateCoupon.php
+++ app/Filament/Resources/CouponResource/Pages/CreateCoupon.php
@@ -40,71 +40,104 @@ class CreateCoupon extends CreateRecord
         }
         if ($checking_the_existence_of_a_coupon == 0 && $data['source'] == 'Ballonozz')
         {
-            $response_coupon = Http::withBasicAuth(env('BALLONOZZ_API_USER_KEY'), env('BALLONOZZ_API_SECRET_KEY'))->get('https://ballonozz.hu/wp-json/wc/v3/orders/'.$data['coupon_code']);
-            //Felőtt(3db->3f): 1567
-            //Családi(1db->2f+2gy): 1508 érvénes
-            //1526 nem érvényes
-            if ($response_coupon->successful())
-            {
-                $coupons_data = $response_coupon->json();
-                $payment_total_price = ($coupons_data['total']);
-                //vásárlás dátumának lekérése
-                $payment_datetime_completed = ($coupons_data['date_completed']);
-                $payment_date_completed = substr($payment_datetime_completed, 0, 10);
-                //kupon lejáratának számítása
-                $payment_date_completed_plus_one_year = strtotime(date("Y-m-d", strtotime($payment_date_completed)) . "+1 year");
-                $coupon_expiration_date = date('Y-m-d', $payment_date_completed_plus_one_year);
-                //kupon felhasználghatóságának türelmi dátumának számítása
-                $payment_date_completed_plus_one_year_plus_one_month = strtotime(date("Y-m-d", strtotime($coupon_expiration_date)) . "+1 month");
-                $coupon_expiration_grace_date = date('Y-m-d', $payment_date_completed_plus_one_year_plus_one_month);
-
-                if ($coupons_data['status'] == 'completed')
+            try {
+                $response_coupon = Http::withBasicAuth(env('BALLONOZZ_API_USER_KEY'), env('BALLONOZZ_API_SECRET_KEY'))->get('https://ballonozz.hu/wp-json/wc/v3/orders/'.$data['coupon_code']);
+            
+                //Felőtt(3db->3f): 1567
+                //Családi(1db->2f+2gy): 1508 érvénes
+                //1526 nem érvényes
+                if ($response_coupon->successful())
                 {
-                    foreach($coupons_data['line_items'] as $coupon)
-                    {
-                        $response_item_nums = $coupon['quantity'];
-                        $response_product_id = $coupon['product_id'];
+                    $coupons_data = $response_coupon->json();
+                    $payment_total_price = ($coupons_data['total']);
+                    //vásárlás dátumának lekérése
+                    $payment_datetime_completed = ($coupons_data['date_completed']);
+                    $payment_date_completed = substr($payment_datetime_completed, 0, 10);
+                    //kupon lejáratának számítása
+                    $payment_date_completed_plus_one_year = strtotime(date("Y-m-d", strtotime($payment_date_completed)) . "+1 year");
+                    $coupon_expiration_date = date('Y-m-d', $payment_date_completed_plus_one_year);
+                    //kupon felhasználghatóságának türelmi dátumának számítása
+                    $payment_date_completed_plus_one_year_plus_one_month = strtotime(date("Y-m-d", strtotime($coupon_expiration_date)) . "+1 month");
+                    $coupon_expiration_grace_date = date('Y-m-d', $payment_date_completed_plus_one_year_plus_one_month);
 
-                        $response_product_attributes = Http::withBasicAuth(env('BALLONOZZ_API_USER_KEY'), env('BALLONOZZ_API_SECRET_KEY'))->get('https://ballonozz.hu/wp-json/wc/v3/products/'.$response_product_id);
-                        if ($response_product_attributes->successful())
+                    if ($coupons_data['status'] == 'completed')
+                    {
+                        foreach($coupons_data['line_items'] as $coupon)
                         {
-                            $product_attributes = $response_product_attributes->json();
-                            $data['tickettype_id'] = ($product_attributes['attributes'][0]['options'][0])*1;
-                            $data['adult'] = ($product_attributes['attributes'][1]['options'][0])*$response_item_nums;
-                            $data['children'] = ($product_attributes['attributes'][2]['options'][0])*$response_item_nums;
-                            $data['status'] = CouponStatus::CanBeUsed;
-                            $data['expiration_at'] = $coupon_expiration_date;
-                            $data['total_price'] = $payment_total_price ;
-                        } else {
-                            Notification::make()
-                                ->title('Hiba történt, kérjük próbálja meg újra később!')
-                                ->color('danger')
-                                ->icon('tabler-alert-triangle')
-                                ->danger()
-                                ->send();
-                            $this->halt();
+                            $response_item_nums = $coupon['quantity'];
+                            $response_product_id = $coupon['product_id'];
+
+                            try {
+                                $response_product_attributes = Http::withBasicAuth(env('BALLONOZZ_API_USER_KEY'), env('BALLONOZZ_API_SECRET_KEY'))->get('https://ballonozz.hu/wp-json/wc/v3/products/'.$response_product_id);
+                                if ($response_product_attributes->successful())
+                                {
+                                    $product_attributes = $response_product_attributes->json();
+                                    $data['tickettype_id'] = ($product_attributes['attributes'][0]['options'][0])*1;
+                                    $data['adult'] = ($product_attributes['attributes'][1]['options'][0])*$response_item_nums;
+                                    $data['children'] = ($product_attributes['attributes'][2]['options'][0])*$response_item_nums;
+                                    $data['status'] = CouponStatus::CanBeUsed;
+                                    $data['expiration_at'] = $coupon_expiration_date;
+                                    $data['total_price'] = $payment_total_price ;
+                                } else {
+                                    Notification::make()
+                                        ->title('Váratlan hiba történt!')
+                                        ->body('Kérjük próbálkozzon újra később.')
+                                        ->color('danger')
+                                        ->icon('tabler-alert-triangle')
+                                        ->danger()
+                                        ->send();
+
+                                    $this->halt();
+                                }
+                            } catch (\Throwable $th) {
+                                if (! $th instanceof \Filament\Support\Exceptions\Halt) {
+                                    Notification::make()
+                                        ->title('Váratlan hiba történt!')
+                                        ->body('Kérjük próbálkozzon újra később.')
+                                        ->color('danger')
+                                        ->icon('tabler-alert-triangle')
+                                        ->danger()
+                                        ->send();
+                                }
+
+                                $this->halt();
+                            }
                         }
                     }
+                    else
+                    {
+                        Notification::make()
+                            ->title('Probléma adódott a megadott kuponkóddal!')
+                            ->body('Bővebb információ érdekében, kérjük vegye fel a kapcsolatot az Ön kuponjának értékesítőjével!')
+                            ->color('danger')
+                            ->icon('tabler-alert-triangle')
+                            ->danger()
+                            ->send();
+
+                        $this->halt();
+                    }
                 }
-                else
-                {
+                else {
                     Notification::make()
-                        ->title('Probléma adódott a megadott kuponkóddal!')
-                        ->body('Bővebb információ érdekében, kérjük vegye fel a kapcsolatot az Ön kuponjának értékesítőjével!')
+                        ->title('Hibás kuponkódott adott meg!')
                         ->color('danger')
                         ->icon('tabler-alert-triangle')
                         ->danger()
                         ->send();
+                        
                     $this->halt();
                 }
-            }
-            else {
-                Notification::make()
-                    ->title('Hibás kuponkódott adott meg!')
-                    ->color('danger')
-                    ->icon('tabler-alert-triangle')
-                    ->danger()
-                    ->send();
+            } catch (\Throwable $th) {
+                if (! $th instanceof \Filament\Support\Exceptions\Halt) {
+                    Notification::make()
+                        ->title('Váratlan hiba történt!')
+                        ->body('Kérjük próbálkozzon újra később.')
+                        ->color('danger')
+                        ->icon('tabler-alert-triangle')
+                        ->danger()
+                        ->send();
+                }
+
                 $this->halt();
             }
         }
