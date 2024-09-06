131f7375eb0d839efb91b8f5fc1bade101d62b6c
Csikós Zsolt <csikoszs21@gmail.com>
2024-09-06 16:57:11 +0200

    feat: command a lejárt státusz módosításához

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('coupon:check-for-expired-coupons')->dailyAt('00:05');
         $schedule->command('queue:work --stop-when-empty')->everyMinute();
         $schedule->command('queue:retry all')->everyFiveMinutes();
     }
