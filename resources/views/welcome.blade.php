f5c2ed43d71b0cec36beace78f356c3bb6af85e3
Csikós Zsolt <csikoszs21@gmail.com>
2024-09-09 10:31:49 +0200

    feat: login gomb a gyűjtőoldalon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,11 @@
     </head>
     <body>
         <div class="container mx-auto flex flex-col gap-6">
+            <nav class="py-2">
+                <ul class="flex justify-end items-center">
+                    <li><a href="{{ route('filament.admin.pages.dashboard') }}">Bejelentkezés</a></li>
+                </ul>
+            </nav>
             <h1 align="center">Üdvözlünk rendszerünkön!</h1>
             <p>Ők már velünk teszik könnyebbé a foglalásaikat:</p>
             <a class="w-min" href="{{ route('ballonozz.home') }}">ballonozz.hu</a>
