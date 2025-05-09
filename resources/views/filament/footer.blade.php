80b08cac9eb36f46ca83d8b1a7f7b8fd5e0f388c
Csikós Zsolt <csikoszs21@gmail.com>
2025-05-09 09:39:23 +0200

    style: footer

--- resources/views/filament/footer.blade.php
+++ resources/views/filament/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="px-4 md:px-6 lg:px-8 w-full">
-    <hr>
-    <div class="flex justify-center text-gray-400 divide-x py-2">
-        <a class="px-2" href="https://ballonozz.hu/felelossegvallalasi-nyilatkozat/" target="_blank">Felelősségvállalási nyilatkozat</a>
-        <a class="px-2" href="https://ballonozz.hu/altalanos-szerzodesi-feltetelek/" target="_blank">ÁSZF</a>
+    <div class="flex justify-center text-gray-400 divide-x py-2" style="border-top: solid 1px rgba(128,128,128,0.2);">
+        <a class="px-2" style="border-color: rgba(128,128,128,0.2);" href="https://ballonozz.hu/felelossegvallalasi-nyilatkozat/" target="_blank">Felelősségvállalási nyilatkozat</a>
+        <a class="px-2" style="border-color: rgba(128,128,128,0.2);" href="https://ballonozz.hu/altalanos-szerzodesi-feltetelek/" target="_blank">ÁSZF</a>
     </div>
 </footer>
